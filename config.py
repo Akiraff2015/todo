001c44ac2d63f78071de1d9bd24a4bf577db6cbc
Akiraff <akiraff@hotmail.com>
2018-02-02 01:36:53 -0200

    Delete config.py and database.db

--- config.py
+++
@@ -1,7 +0,0 @@
-from custom_class.load_json import open_file
-
-DEBUG = True
-TEMPLATES_AUTO_RELOAD = True
-SECRET_KEY = open_file('key.json', 'key', 'SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = open_file('key.json', 'db', 'url')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
